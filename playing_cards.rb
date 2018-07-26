a9bd76c6f540b08eb398f2accb1cad0c3c33b471
Jack Burum <jtburum@gmail.com>
2018-07-26 15:55:31 -0500

    add to deck of cards

--- playing_cards.rb
+++ playing_cards.rb
@@ -1,8 +1,3 @@
 require_relative 'lib/deck'
 require_relative 'lib/hand'
 require_relative 'lib/card'
-
-# deck = PlayingCards::Deck.new
-# hand = PlayingCards::Hand.new({ deck: deck, draw_count: 2})
-# hand.draw
-# require "/Users/jackburum/interview-coding-challenges/deck_of_cards/playing_cards"
